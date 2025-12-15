06700c458160620f841528c26dfcd6535943eb01
AxlOlvera <axl.sanchezolvera@gmail.com>
2025-12-15 15:07:52 -0600

    Se corrigió el ejercicio task4.js

--- task4.js
+++ task4.js
@@ -4,13 +4,23 @@
 // Refer to Task 1 in your Instructions to complete this task
 const prompt = require("prompt-sync")();
 const name = prompt("Ingresa tu nombre: ");
+let lineas;
+
 console.log(`Hola ${name}`);
+lineas = prompt("Ingresa la cantidad de líneas que quieres imprimir")
+
 
 
 for(let i = 1; i < 151; i++){
 
   if(i % 3 === 0 && i % 5 === 0 && i % 7 === 0){
+    console.log("FizzBuzz");
+  }else if(i % 3 === 0 && i % 5 === 0){
+    console.log("BuzzWoof");
+  }else if(i % 5 === 0 && i % 7 === 0){
     console.log("FizzBuzzWoof");
+  }else if(i % 3 === 0 && i % 7 === 0){
+    console.log("FizzWoof");
   }else if(i % 3 === 0){
     console.log("Fizz");
   }else if(i % 5 === 0){
--- task5.js
+++ task5.js
@@ -1,5 +1,33 @@
 // Refer to Task 5 in your Instructions to complete this task
+// Refer to Task 4 in your Instructions to complete this task
+// Refer to Task 3 in your Instructions to complete this task
+// Refer to Task 2 in your Instructions to complete this task
+// Refer to Task 1 in your Instructions to complete this task
+const prompt = require("prompt-sync")();
+const name = prompt("Ingresa tu nombre: ");
+let lineas;
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Five!");
-  };
+console.log(`Hola ${name}`);
+lineas = prompt("Ingresa la cantidad de líneas que quieres imprimir");
+
+
+
+
+
+for(let x = 1; x < lineas; x++){
+
+  if(x % 3 === 0 && x % 5 === 0 && x % 7 === 0){
+    console.log("FizzBuzzWoof");
+  }else if(x % 3 === 0){
+    console.log("Fizz");
+  }else if(x % 5 === 0){
+    console.log("Buzz");
+  }else if(x % 7 === 0){
+    console.log("Woof");
+  }else{
+    console.log(x)
+  }
+
+
+
+}
